bc5f36eb4368cbc5ba17b70e188902940b7036bc
Naveen JOHNSON VALLAVANATT <naveenjohnsonv@gmail.com>
2025-11-12 17:43:16 +0100

    Set PDF margins to 0.4 inches (approximately 1 cm)

--- .github/scripts/html-to-pdf.js
+++ .github/scripts/html-to-pdf.js
@@ -24,10 +24,10 @@ async function convertHtmlToPdf(htmlPath, pdfPath) {
     format: 'A4',
     printBackground: true,
     margin: {
-      top: 0,
-      right: 0,
-      bottom: 0,
-      left: 0
+      top: '0.4in',
+      right: '0.4in',
+      bottom: '0.4in',
+      left: '0.4in'
     }
   });
   
